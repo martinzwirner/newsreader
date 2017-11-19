15a1244c3505d02a8189e1cc3c89e319572f1596
Martin Zwirner <mz@matrix-architekt.de>
2017-11-19 18:19:41 +0100

    - fixes - added processing - added host model - improved logging formatting

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 "use strict";
 
+const process = require('process');
+const processor = require('./lib/processing/processor');
+const logger = require('./lib/logger');
 const Http = require('./lib/http');
-const importer = require('./lib/importer');
+const importer = require('./lib/import/importer');
 const Database = require('./lib/database');
 const PocketHtmlExport = require('./lib/import/pocket/HtmlExport');
+
+process.on('unhandledRejection', function(reason, p){
+  console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
+  logger.error("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
+});
 
 Database.connect()
 .then(() => {
 
-  importer.import(PocketHtmlExport);
+  logger.debug('db connection established');
+
+  importer.import(PocketHtmlExport)
+  .then(() => {
+    logger.debug('import done');
+    processor.startAsync();
+  });
+
+  logger.debug('import started');
 
   new Http({port: 8080});
 });
